678e522ed48645de9d9e82db53f3a026c255bf9c
David Syengo <david.syengo019@gmail.com>
2025-01-28 14:14:22 +0300

    wip: added scene transitions via UI buttons

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void GoToMenuScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
