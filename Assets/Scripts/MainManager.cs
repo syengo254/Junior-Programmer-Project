071ecbfadcdfb2abe66d64d0520fa40e0bbea91d
David Syengo <david.syengo019@gmail.com>
2025-01-28 14:47:12 +0300

    wip: data persistence between scenes

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static MainManager Instance;
+    public Color TeamColor;
+    
+    private void Awake() {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
